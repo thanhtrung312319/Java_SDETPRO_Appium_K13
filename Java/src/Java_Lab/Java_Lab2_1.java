394e6ec8bdec3ddf68dff14037b6a3717de84581
Admin <thanhtrung312319@gmail.com>
2024-01-16 21:56:22 +0700

    Java lab2 updated

--- Java/src/Java_Lab/Java_Lab2_1.java
+++ Java/src/Java_Lab/Java_Lab2_1.java
@@ -13,13 +13,13 @@ public class Java_Lab2_1 {
         float clientBMI = clientWeight /(clientHight*2);
         System.out.println("Your BMI is: " + clientBMI);
         if (clientBMI <= 18.5){
-            System.out.println("You'r Under weight");
+            System.out.println("You're Under weight");
         } else if (clientBMI >= 18.5 && clientBMI <= 24.9) {
-            System.out.println("You'r Normal weight");
+            System.out.println("You're Normal weight");
         } else if (clientBMI >= 25 && clientBMI <= 29.9)
-        System.out.println("You'r Overweight");
+        System.out.println("You're Overweight");
         else if (clientBMI >= 30)
-        System.out.println("You'r Obesity");
+        System.out.println("You're Obesity");
 
     }
 }
