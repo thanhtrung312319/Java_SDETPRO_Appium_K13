50bc1401f4fba8a3e29f0b7307fe8b643de29816
Admin <thanhtrung312319@gmail.com>
2024-01-21 19:59:07 +0700

    updated

---
+++ Java/src/Java_Lab/Java_Lab3_1.java
@@ -0,0 +1,18 @@
+package Java_Lab;
+import java.util.Arrays;
+public class Java_Lab3_1 {
+    public void main(String[]args) {
+        int[] intArr = {1, 5, 3, 7, 6, 4};
+
+        int countOdd = 0;
+        int countEven = 0;
+        for (int i = 0; i < intArr.length; i++) {
+            if (intArr[i] % 2 == 0)
+                countEven++;
+            else countOdd++;
+        }
+
+        System.out.println(Arrays.toString(intArr));
+        System.out.printf("\nThis array has %d even number(s), %d odd number(s) \n", countEven, countOdd);
+    }
+}
